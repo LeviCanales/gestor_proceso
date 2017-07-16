83d5a9c9cf2e31dfd5e7bcf8496d476e5ecd0f37
LeviCanales <milevi271@gmail.com>
2017-07-15 18:22:14 -0600

    Solo inicio de lectura

--- index.php
+++ index.php
@@ -5,52 +5,22 @@
 	include("class/class-proceso.php");
 
 	$probarMemoria = new Lista();
-	$probarMemoria->getContenedor()->memoria();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Simulador Procesos</title>
 </head>
 <body>
 <?php
-	/*$probarMemoria->memoria();
-	$boo = false;
-	if ($boo) {
-		echo "# code...";
+	$direccion = "data/procesos.txt";
+	if(file_exists($direccion)){
+		$archivo =  fopen($direccion, "r") or die("No se pudo abrir");
+		echo fread($archivo, filesize($direccion));
+		fclose($archivo);
+	}else{
+		echo "No se encontro proceso.";
 	}
-	echo '<br>'.'H: '.$boo . '<br>';*/
-		$probarMemoria->INSERTA("1050", $probarMemoria->PRIMERO());
-		$probarMemoria->INSERTA("1072", $probarMemoria->ANTERIOR($probarMemoria->FIN()));
-		$probarMemoria->INSERTA("1144", $probarMemoria->PRIMERO());
-		$probarMemoria->INSERTA("1378", $probarMemoria->SIGUIENTE($probarMemoria->PRIMERO()));
-		echo '<br>'."Vacia? " . $probarMemoria->VACIA(). " FIN:".$probarMemoria->FIN().'<br>';
-		$probarMemoria->INSERTA("1391", $probarMemoria->SIGUIENTE($probarMemoria->PRIMERO()));
-		$probarMemoria->getContenedor()->imprimeLista();
-		echo '<br>'."Vacia? " . $probarMemoria->PRIMERO(). " Localiza:".$probarMemoria->LOCALIZA("1378").'<br>';
-		$probarMemoria->getContenedor()->imprimeLista();
-		echo '<br>'."Vacia? " . $probarMemoria->VACIA(). " FIN:".$probarMemoria->FIN();
-		$probarMemoria->ANULA();
-		$probarMemoria->INSERTA("1050", $probarMemoria->PRIMERO());
-		$probarMemoria->INSERTA("1072", $probarMemoria->SIGUIENTE($probarMemoria->PRIMERO()));
-		$probarMemoria->INSERTA("1144", $probarMemoria->PRIMERO());
-		$probarMemoria->INSERTA("1378", $probarMemoria->ANTERIOR($probarMemoria->FIN()));
-		$probarMemoria->getContenedor()->imprimeLista();
-		echo '<br>'."Vacia? " . $probarMemoria->VACIA(). " FIN:".$probarMemoria->FIN();
-		//$probarMemoria->ANULA();
-		$probarMemoria->INSERTA("1391", $probarMemoria->SIGUIENTE($probarMemoria->PRIMERO()));
-		echo '<br>'."Vacia? " . $probarMemoria->PRIMERO(). " Localiza:".$probarMemoria->LOCALIZA("1378").'<br>';
-		$probarMemoria->getContenedor()->imprimeLista();
-		echo '<br>'."Vacia? " . $probarMemoria->VACIA(). " FIN:".$probarMemoria->FIN();
-		echo '<br>'."Recupera " . $probarMemoria->RECUPERA($probarMemoria->ANTERIOR($probarMemoria->FIN()));
-		echo '<br>'."Vacia? " . $probarMemoria->PRIMERO(). " Localiza:".$probarMemoria->LOCALIZA("1072").'<br>';
-		echo '<br>'."<<<<";
-		$probarMemoria->getContenedor()->imprimeLista();
-		$probarMemoria->SUPRIME($probarMemoria->FIN());
-		echo '<br>'."<<<<";
-		$probarMemoria->getContenedor()->imprimeLista();
-		echo '<br>'."Vacia? " . $probarMemoria->VACIA(). " FIN:".$probarMemoria->FIN();
-		echo '<br>'."Vacia? " . $probarMemoria->PRIMERO(). " Localiza:".$probarMemoria->LOCALIZA("1378")."\n".'<br>';
 ?>
 </body>
 </html>
