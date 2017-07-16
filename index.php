06326109df9bc1c6a0b9f8e833fe92c5f7c87f49
LeviCanales <milevi271@gmail.com>
2017-07-15 23:08:11 -0600

    Apariencia

--- index.php
+++ index.php
@@ -3,15 +3,26 @@
 	include("class/class-cursor.php");
 	include("class/class-lista.php");
 	include("class/class-proceso.php");
-
 	$probarMemoria = new Lista();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<style>
+	.cuadro-transparente{
+        background-color: rgba(0,0,0,0.5);
+        border: 1px; border-color: #ffffff;
+        border-style: solid;
+        padding: 20px;
+        margin: 20px;
+        font-family:'Orbitron', sans-serif;
+      }
+      .highlight { color: black; background-color: red; padding-left: 20px; padding-right: 20px;}
+	</style>
 	<title>Simulador Procesos</title>
 </head>
-<body style="font-size: 40px">
+<body style="color:#ffffff;background-image: url(img/thumb-1920-593483.jpg);background-size: cover;background-attachment: fixed; font-size: 20px">
 <?php
 	$direccion = "data/procesos.txt";
 	if(file_exists($direccion)){
@@ -23,20 +34,41 @@
 		fwrite($txt, $todos);
 		fclose($txt);*/
 		$proceso = explode(";", $todos, -1);
-		echo '<span style="background-color:rgba(255,0,50,0.6);"> Tamaño: '.sizeof($proceso).'<br>';
+		echo "<div class='grid'>";
 		for ($i=0; $i < sizeof($proceso); $i++) {
+			echo '<div class="grid-item cuadro-transparente">';
 			echo $i.': '.$proceso[$i].'<br>';
 			$dato = explode("/", $proceso[$i]);
 			for ($j=0; $j < sizeof($dato); $j++) { 
-				echo "* ".$j.': '.$dato[$j].' is_numeric(): '.((is_numeric($dato[$j]))?'True':'False').'<br>';
+				echo "* ".$j.': '.$dato[$j].' is_numeric(): '.((is_numeric($dato[$j]))?'True':'False').' son 6?: '.((sizeof($dato)==6)?'True':'False').'<br>';
 			}
-			echo "<br>";
+			echo "</div><br>";
 		}
-		echo "</span>";
+		echo "</div>";
 		fclose($archivo);
 	}else{
 		echo "No se encontro proceso.";
 	}
 ?>
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.highlight-5.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/masonry.pkgd.min.js"></script>
+<script src="js/imagesloaded.pkgd.min.js"></script>
+<script>
+var $grid = $('.grid').imagesLoaded( function() {
+  // init Masonry after all images have loaded
+  $grid.masonry({
+    columnWidth: 40
+  });
+});
+$grid.on( 'click', '.grid-item', function() {
+  // remove clicked element
+  $grid.masonry( 'remove', this )
+    // layout remaining item elements
+    .masonry('layout');
+});
+$('.grid').highlight('False');
+</script>
 </body>
 </html>
