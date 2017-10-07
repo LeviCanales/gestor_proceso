46174e5a07b2e2f0fd1000f057218c017436e949
LeviCanales <milevi271@gmail.com>
2017-10-06 18:14:50 -0600

    ah, este tambien xD

--- ajax/acciones.php
+++ ajax/acciones.php
@@ -26,30 +26,61 @@
 				$listo->RECUPERA($p+$i)->setEstado(1);
 				echo '<br>'.($i+1).': '.$listo->RECUPERA($p+$i)->proceso();
 			}
+			echo '<br><br>';
 			//CICLO PRINCIPAL
 			$contadorTres = 0;
 			$seRepite = null;
+			$buscarProceso = true;
+			$contadorCinco = 0;
+			$contadorSegmento = 1;
+			echo '<div class="row">';
 			for ($i=0; $i < $_POST["numero_ciclos"]; $i++) {
 				//Ver si se repite el proceso elegido seguidamente
-				if ($seRepite == $listo->RECUPERA($p)->getId_proceso()) {
-					$contadorTres++;
-				}else{
-					$contadorTres = 0;
+				if ($buscarProceso) {
+					if ($seRepite == $listo->RECUPERA($p)->getId_proceso()) {
+						$contadorTres++;
+					}else{
+						$contadorTres = 0;
+					}
+					//si seguidamente lleva 3 veces.
+					if ($contadorTres == 3) {
+						if ($listo->RECUPERA($p)->getPrioridad()!=3) {
+							$listo->RECUPERA($p)->setPrioridad((($listo->RECUPERA($p)->getPrioridad())+1));
+						}
+						ordenarBurbuja($listo);
+						if ($seRepite == $listo->RECUPERA($p)->getId_proceso()) {
+							if ($listo->PRIMERO()!=($listo->FIN()-1)) {
+								$listo->INSERTA($listo->RECUPERA($p),$listo->SIGUIENTE($listo->SIGUIENTE($p)));
+								$listo->SUPRIME($p);
+							}
+						}
+						$contadorTres = 0;
+					}
 				}
-				//si seguidamente lleva 3 veces.
-				if ($contadorTres == 3) {
-					$listo->RECUPERA($p)->setPrioridad((($listo->RECUPERA($p)->getPrioridad())+1));
-					ordenarBurbuja($listo);
-					$contadorTres = 0;
+				//Segmento de 5 ciclos
+				if ($contadorCinco<5) {
+					$listo->RECUPERA($p)->setEstado(2);
+					$listo->RECUPERA($p)->setNum_instruccion(1+($listo->RECUPERA($p)->getNum_instruccion()));
+					$buscarProceso = false;
+					$contadorCinco++;
 				}
-				echo '<br>Ciclo: '.($i+1).': '.$contadorTres.'. ID:'.$listo->RECUPERA($p)->getId_proceso();
+				if (!($contadorCinco<5)) {
+					$buscarProceso = true;
+					$contadorCinco = 0;
+				}
+				echo '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4" id="borde">Segmento: '.$contadorSegmento.'<br>Ciclo: '.($i+1).': Repetido: '.$contadorTres.'. ID:'.$listo->RECUPERA($p)->getId_proceso();
 				//Solo para ver los cambios en las prioridades.
 				for ($z=0; $z < ($listo->FIN()-1); $z++) {
-					echo '<br>&nbsp&nbsp'.($z+1).': '.$listo->RECUPERA($p+$z)->proceso();
+					echo '<br>&nbsp&nbsp'.($z+1).': '.$listo->RECUPERA($p+$z)->proceso().'<br>&nbsp&nbsp&nbsp'.$listo->RECUPERA($p+$z)->estado();
 				}
-				$seRepite = $listo->RECUPERA($p)->getId_proceso();
+				echo '</div>';
+				if ($buscarProceso) {
+					$seRepite = $listo->RECUPERA($p)->getId_proceso();
+					$listo->RECUPERA($p)->setEstado(1);
+					$contadorSegmento++;
+				}
 			}
-			echo '</div>';
+			echo '</div></div>';
 			break;
 		
 		default:
